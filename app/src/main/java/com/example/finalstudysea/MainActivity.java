d2fd015d42fe11c91d1aa1579f4769eb3d5b3656
tewari6850 <tewarisuyash@gmail.com>
2019-11-05 14:00:42 -0600

    Initial commit

---
+++ app/src/main/java/com/example/finalstudysea/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.finalstudysea;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button toFlashcards, toTasks;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toFlashcards = (Button) findViewById(R.id.flashcards);
+        toTasks = (Button) findViewById(R.id.tasks);
+
+        toFlashcards.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, FlashcardActivity.class);
+            }
+        });
+    }
+}
